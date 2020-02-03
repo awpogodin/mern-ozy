f5b30cff24d8b8d9bcdb3019750f3357af10e98c
Alexandr <awpogodin@mail.ru>
2020-02-03 18:18:15 +0400

    // Update // fixed build errors

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,6 +1,6 @@
+import isEmpty from 'is-empty';
 import { SET_CURRENT_USER, USER_LOADING } from '../actions/types';
 
-const isEmpty = require('is-empty');
 
 const initialState = {
   isAuthenticated: false,
